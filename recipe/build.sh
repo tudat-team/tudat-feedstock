0b01fcdec14753c03f8a6a906573b3896429b124
Geoffrey Garrett <g.h.garrett13@gmail.com>
2020-06-14 19:47:01 +0200

    Initial feedstock commit with conda-smithy 3.7.4.dev54.

---
+++ recipe/build.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+mkdir build
+
+cd build
+
+cmake \
+    -DCMAKE_CXX_STANDARD=17 \
+    -DCMAKE_BUILD_TYPE=Release \
+    -DCMAKE_INSTALL_PREFIX=$PREFIX \
+    -DCMAKE_PREFIX_PATH=$PREFIX \
+    -DPREFIX=$PREFIX \
+    -DTUDAT_CONDA_BUILD=on \
+    -DTUDAT_BUILD_STATIC_LIBRARY=off \
+    -DTUDAT_BUILD_TUDAT_TUTORIALS=off \
+    -DTUDAT_BUILD_WITH_SOFA_INTERFACE=on \
+    -DTUDAT_BUILD_WITH_SPICE_INTERFACE=on \
+    -DTUDAT_INSTALL=on \
+    -DTUDAT_TEST_INSTALL=off \
+    ..
+
+make
+
+make install
