16074ca83ae279117611429b09d12d817581e55f
Geoffrey Garrett <g.h.garrett13@gmail.com>
2020-09-07 12:04:55 +0200

    Updated hash and disabled estimation for macOS

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -4,20 +4,20 @@ cd build
 if errorlevel 1 exit 1
 cmake ^
     -G "MinGW Makefiles" ^
-    -DCMAKE_CXX_STANDARD=17 ^
+    -DCMAKE_CXX_STANDARD=14 ^
     -DCMAKE_BUILD_TYPE=Release ^
     -DCMAKE_PREFIX_PATH=%LIBRARY_PREFIX% ^
     -DCMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% ^
     -DPREFIX=%LIBRARY_PREFIX% ^
-    -DTUDAT_CONDA_BUILD=on ^
     -DTUDAT_BUILD_STATIC_LIBRARY=on ^
     -DTUDAT_BUILD_TUDAT_TUTORIALS=off ^
-    -DTUDAT_BUILD_TESTS=off ^
+    -DTUDAT_BUILD_TESTS=on ^
     -DTUDAT_BUILD_WITH_SOFA_INTERFACE=on ^
     -DTUDAT_BUILD_WITH_SPICE_INTERFACE=on ^
+    -DTUDAT_BUILD_WITH_JSON_INTERFACE=off ^
     ..
 if errorlevel 1 exit 1
 cmake --build . --config RelWithDebInfo --target install -- -j4
 if errorlevel 1 exit 1
-ctest
+ctest --verbose
 if errorlevel 1 exit 1
