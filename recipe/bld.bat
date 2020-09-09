549644e63c4597da17e758ed5474e3a053af5394
Geoffrey Hyde Garrett <g.h.garrett13@gmail.com>
2020-09-09 17:06:49 +0200

    update bld.bat

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -3,7 +3,7 @@ if errorlevel 1 exit 1
 cd build
 if errorlevel 1 exit 1
 cmake ^
-    -G "%CMAKE_GEN%" ^    -DCMAKE_CXX_STANDARD=17 ^
+    -DCMAKE_CXX_STANDARD=17 ^
     -DCMAKE_BUILD_TYPE=Release ^
     -DCMAKE_PREFIX_PATH=%LIBRARY_PREFIX% ^
     -DCMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% ^
