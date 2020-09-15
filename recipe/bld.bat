28b9544577b4c15f5108e042c5920b3e0b301a0f
Geoffrey Garrett <g.h.garrett13@gmail.com>
2020-09-15 15:48:10 +0200

    Attempt at vs2019

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -51,7 +51,7 @@ mkdir build
 cd build
 
 cmake ^
-    -G "%CMAKE_GEN%" ^
+    -G "Visual Studio 16 2019 Win64" ^
     -DCMAKE_CXX_STANDARD=17 ^
     -DCMAKE_PREFIX_PATH=%LIBRARY_PREFIX% ^
     -DCMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% ^
