539d1d37eb25ab20db59cb7d15dd2e2366234106
Geoffrey Garrett <g.h.garrett13@gmail.com>
2020-09-15 23:48:16 +0200

    Modified compiler definition in bld.bat (win)

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -51,6 +51,9 @@ mkdir build
 cd build
 
 cmake ^
+    -G "%CMAKE_GEN%" ^
+    -DCMAKE_C_COMPILER=%BUILD_PREFIX%\Library\bin\clang ^
+    -DCMAKE_CXX_COMPILER=%BUILD_PREFIX%\Library\bin\clang++ ^
     -DCMAKE_CXX_STANDARD=17 ^
     -DCMAKE_PREFIX_PATH=%LIBRARY_PREFIX% ^
     -DCMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% ^
