008a9daf810e2c3d2e0500da88dc3905fb0f76d6
Geoffrey H. Garrett <g.h.garrett13@gmail.com>
2020-09-22 10:02:02 +0200

    Attempt at explicit definition of clang_rt under \link

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -10,8 +10,8 @@ cmake ^
     -DCMAKE_C_COMPILER="clang-cl.exe" ^
     -DCMAKE_CXX_COMPILER="clang-cl.exe" ^
     -DCMAKE_CXX_STANDARD=17 ^
-    -DCFLAGS="%CFLAGS%" ^
-    -DCXXFLAGS="%CXXFLAGS%" ^
+    -DCFLAGS="%CFLAGS% \link -Wl,-defaultlib:%BUILD_PREFIX%/lib/clang/9.0.1/lib/windows/clang_rt.builtins-x86_64.lib" ^
+    -DCXXFLAGS="%CXXFLAGS% \link -Wl,-defaultlib:%BUILD_PREFIX%/lib/clang/9.0.1/lib/windows/clang_rt.builtins-x86_64.lib" ^
     -DCMAKE_PREFIX_PATH=%LIBRARY_PREFIX% ^
     -DCMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% ^
     -DTUDAT_BUILD_TESTS=1 ^
