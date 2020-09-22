dce9282a8cfa04c64d303d942f051b45e62a6838
Geoffrey H. Garrett <g.h.garrett13@gmail.com>
2020-09-22 07:41:51 +0100

    update bld.bat

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -6,12 +6,12 @@ cd build
 set "BUILD_PREFIX=%BUILD_PREFIX:\=/%"
 
 cmake ^
-    -G "NMake Makefiles" ^
+    -G "Ninja" ^
     -DCMAKE_C_COMPILER="clang-cl.exe" ^
     -DCMAKE_CXX_COMPILER="clang-cl.exe" ^
     -DCMAKE_CXX_STANDARD=17 ^
-    -DCFLAGS="%CFLAGS% /link %LDFLAGS%" ^
-    -DCXXFLAGS="%CXXFLAGS% /link %LDFLAGS%" ^
+    -DCFLAGS="%CFLAGS%" ^
+    -DCXXFLAGS="%CXXFLAGS%" ^
     -DCMAKE_PREFIX_PATH=%LIBRARY_PREFIX% ^
     -DCMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% ^
     -DTUDAT_BUILD_TESTS=1 ^
