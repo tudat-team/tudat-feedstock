e4d2b16ca76a1655e93bdaeead00a3a9398b46e3
Dominic Dirkx <d.dirkx@tudelft.nl>
2024-07-02 10:53:17 +0200

    Corrected hard-coded clang

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -8,7 +8,7 @@ set "BUILD_PREFIX=%BUILD_PREFIX:\=/%"
 
 cmake ^
     -G "NMake Makefiles" ^
-    -DCMAKE_CXX_COMPILER=%BUILD_PREFIX%/Library/bin/clang.exe ^
+    -DCMAKE_CXX_COMPILER=%BUILD_PREFIX%/Library/bin/clang++.exe ^
     -DCMAKE_CXX_STANDARD=17 ^
     -DCMAKE_PREFIX_PATH=LIBRARY_PREFIX% ^
     -DCMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% ^
